aa8dd2a7812040acb5bd15c9eb29669fde34153c
Salledelavage <salledelavager@gmail.com>
2023-07-09 09:56:48 -0400

    adding automatic update

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # linux_manager
 
 Automate common programming tasks on Linux
+
+Default name for the binary is 'mgr', the script handles changes in the name so rename it to anything you like. The installation scripts does not manage changes in the name though.
+
+# Installation
+
+Change permissions on installation.sh
+
+```
+chmod 775 installation.sh
+```
+
+Lauch script
+
+```
+./installation.sh
+```
---
+++ installation.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/sh
+
+function ProgressBar {
+# Process data
+	let _progress=(${1}*100/${2}*100)/100
+	let _done=(${_progress}*4)/10
+	let _left=40-$_done
+# Build progressbar string lengths
+	_done=$(printf "%${_done}s")
+	_left=$(printf "%${_left}s")
+
+# 1.2 Build progressbar strings and print the ProgressBar line
+# 1.2.1 Output example:
+# 1.2.1.1 Progress : [########################################] 100%
+printf "\r ${3} : [${_done// /#}${_left// /-}] ${_progress}%%\n"
+
+}
+
+# Variables
+_start=1
+
+# This is the amount of tasks that needs to be done in the installation
+_end=2
+
+# Proof of concept
+for number in $(seq ${_start} ${_end})
+do
+	sleep 1
+
+    # Changes permissions on mgr
+    if [[ $number == 1 ]]; then
+        steps="Setting_permissions"
+        chmod 711 mgr
+    fi
+
+    # Copies mgr binary for local user
+    if [[ $number == 2 ]]; then
+        steps="Copying_files"
+        cp mgr $HOME/.local/bin
+    fi
+
+	ProgressBar ${number} ${_end} ${steps}
+done
+
+echo "program is installed here: " && which mgr
